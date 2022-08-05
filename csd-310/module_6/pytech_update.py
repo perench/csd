d4aefddcb631c54acef8893f8152890f690983cb
perench <80637801+perench@users.noreply.github.com>
2022-08-04 18:33:11 -0700

    commit

--- csd-310/module_6/pytech_update.py
+++ csd-310/module_6/pytech_update.py
@@ -1,7 +1,15 @@
 import pymongo
 from pymongo import MongoClient 
-client=MongoClient("mongodb+srv://admin:admin@cluster0.cnec1ir.mongodb.net/?retryWrites=true&w=majority")
+client= MongoClient("mongodb+srv://admin:admin@cluster0.cnec1ir.mongodb.net/?retryWrites=true&w=majority")
 db= client["pytech"]
 collection= db["students"]
 
-result= db.collection.update_one({"student_id":1007}), {"$set": {"last_name":"Henry"}}
+post1={"_id": 1007, "First Name":"Andrew", "Last Name":"Rench"}
+post2={"_id": 1008, "First Name": "Peyton", "Last Name": "Rench"}
+post3={"_id": 1009, "First Name": "Alyssa", "Last Name": "Rench"}
+
+#collection.insert_one(post1)
+
+collection.insert_many([post1,post2,post3])
+
+db.sample.update({"_id":"1007"},{"$set":{"Last Name":"Henry"}})
